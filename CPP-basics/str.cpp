206a0b55d0335bf77609607cb7722ad8c2e36170
PrinceMalviyaDev <pm306616@gmail.com>
2026-02-12 21:27:30 +0530

    revise the cpp strings

---
+++ CPP-basics/str.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "Prince";
+    int len = s.size();  // returns the length of the array
+    cout << "Length of the array: " << len;
+    return 0;
+}
