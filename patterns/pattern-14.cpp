9ae1144b7a1a1859ba6384b550fd1c8efe81dad1
PrinceMalviyaDev <pm306616@gmail.com>
2026-05-16 23:59:45 +0530

    learn pattern 14

---
+++ patterns/pattern-14.cpp
@@ -0,0 +1,38 @@
+// pattern - 14:
+
+/* 
+
+for n = 5
+
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n){
+    for(int i = 0; i < n; i++){
+        char ch = 'A';
+        for(int j = 0; j <= i; j++){
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+        pattern(n);
+    }
+    return 0;
+}
