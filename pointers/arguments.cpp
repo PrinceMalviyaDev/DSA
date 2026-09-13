ab002bfac4991ec6a92678042ecfa5dc5e9dbdcd
PrinceMalviyaDev <pm306616@gmail.com>
2026-09-13 23:03:16 +0530

    learn reference variable

--- pointers/arguments.cpp
+++ pointers/arguments.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
+// pass by value
 void changeA(int a) {
     a = 20;
     cout << a << endl;  // 20
 }
 
+// pass by referrence using pointer
 void changeB(int *a) {
     *a = 30;
 }
 
+//pass by referrence using referrence variable
+void changeC(int &a) {
+    a = 40;
+}
 int main(){
     int a = 10;
     int *ptr = &a;
@@ -17,8 +23,11 @@ int main(){
     changeA(a);  // pass by value
     cout << a << endl;   // 10
 
-    changeB(ptr);  // pass by reference
-    cout << a;   // 30
+    changeB(ptr);  // pass by reference using pointer
+    cout << a << endl;   // 30
+
+    changeC(a);   // pass by reference using reference variable
+    cout << a; //40
 
     return 0;
 }
